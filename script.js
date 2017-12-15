0058db7b7b505bb418415ca139fae6c49a9e88ea
vovoapps <31407451+vovoapps@users.noreply.github.com>
2017-12-15 18:19:25 +0500

    test 9

--- script.js
+++ script.js
@@ -136,7 +136,8 @@
 		tab_click(0);
 		//Android.showtoast("Aslam Walikum");
 		//Android.setPremium();
-		Android.dilouge("<b>Aslam Walikm<b><br>this the message");
+		//Android.dilouge("<b>Aslam Walikm<b><br>this the message");
+		Android.setNotification("Aslam|walkum");
 	}
 	
 	

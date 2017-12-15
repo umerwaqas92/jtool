b900beebb862da8dd5cd020e6fe9ed406bcec59b
vovoapps <31407451+vovoapps@users.noreply.github.com>
2017-12-15 18:39:43 +0500

    set premium

--- script.js
+++ script.js
@@ -135,9 +135,9 @@
 	function onsload(){
 		tab_click(0);
 		//Android.showtoast("Aslam Walikum");
-		//Android.setPremium();
+		Android.setPremium();
 		//Android.dilouge("<b>Aslam Walikm<b><br>this the message");
-		Android.setNotification("Aslam,walkum");
+		//Android.setNotification("Aslam,walkum");
 	}
 	
 	
